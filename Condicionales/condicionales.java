d27569955c71672b38090489b0b65d57c1dbfb39
paulaabc4406 <145506507+paulaabc4406@users.noreply.github.com>
2023-09-24 19:00:54 -0500

    Add files via upload

---
+++ Condicionales/condicionales.java
@@ -0,0 +1,14 @@
+package conditionalexamples;
+
+public class ConditionalExamples {
+
+    public static void main(String[] args) {
+        int x = 10;    
+        int y = 12;  
+
+        if(x+y > 20) {    
+            System.out.println("x + y is greater than 20");    
+
+        }    
+    }      
+}
