4705f5f983d79781e0bf1001d08545b37ce16af7
paulaabc4406 <145506507+paulaabc4406@users.noreply.github.com>
2023-11-13 18:03:02 -0500

    Add files via upload

---
+++ segundo corte/Arduinando/TestArduino.java
@@ -0,0 +1,15 @@
+import jssc.SerialPort;
+import jssc.SerialPortList;
+
+
+public class TestArduino {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String puertos[] = SerialPortList.getPortNames();
+        
+        for (String n : puertos) {
+         System.out.println(n);
+        }
+    }    
+}
