d42ddfe5e8ccf107ed00f0e8e548512cbf22a432
paulaabc4406 <145506507+paulaabc4406@users.noreply.github.com>
2023-11-18 23:11:58 -0500

    Update Welcome.java

--- segundo corte/abstracts/Welcome.java
+++ segundo corte/abstracts/Welcome.java
@@ -3,9 +3,6 @@ package welcome;
 import javax.swing.JOptionPane;
 public class Welcome {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"¡Welcome\nto\nthe java word!");
         System.exit(0);
